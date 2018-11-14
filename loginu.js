e169a27908722cbd1d79273fc700f496d369d35c
lbratto1 <43048252+lbratto1@users.noreply.github.com>
2018-11-14 12:52:44 -0500

    Add files via upload

---
+++ loginu.js
@@ -0,0 +1,52 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login() {
+    window.alert("working!"); //to test and see if the login button works!
+  var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+   // window.alert(userEmail + " " + userPass);
+
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+function goHome() {
+    window.location = "driver/home.html";
+
+}
